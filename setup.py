e81ead6d5ac973959c0dbf9d70d3a9ec67edacfa
Your <edu.alri@gmail.com>
2024-02-28 11:02:20 -0300

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Management-Public Chatbot',
+    version= '0.0.0',
+    author= 'Eduardo Ribeiro',
+    author_email= 'edu.alri@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
